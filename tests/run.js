5f8db19592f4cdc0a4490e5065ff6d1936b8cc81
rugk <rugk+git@posteo.de>
2019-07-08 15:04:49 +0200

    Rename test file

--- tests/run.js
+++ tests/run.js
@@ -1,7 +1,7 @@
 import "https://unpkg.com/mocha@5.2.0/mocha.js"; /* globals mocha */
 
 /* tests */
-import "./environmentDetector.test.js";
+import "./helper.test.js";
 
 mocha.checkLeaks();
 mocha.run();
